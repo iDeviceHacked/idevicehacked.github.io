a4156dc2504afc3a3ba9d7a9c502c92a3fc0041f
Isaac <luciferscircle@gmail.com>
2019-03-08 15:23:26 -0800

    just added text to script so you know whats going on

--- update.sh
+++ update.sh
@@ -1,2 +1,7 @@
-dpkg-scanpackages -m noDebsHere / > Packages
+echo -e "\e[31mStarting Scan...\e[0m"
+dpkg-scanpackages -m noDebsHere/ / > Packages
+echo -e "\e[31mComplete...\e[0m"
+
+echo -e "\e[31mBuilding Packages File...\e[0m"
 bzip2 -fks Packages
+echo -e "\e[31mComplete... You can now push to git!!\e[0m"
