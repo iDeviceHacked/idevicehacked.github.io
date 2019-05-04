5fac632f24684223e8769f49d5845da0253f8e88
LucifersCircle <luciferscircle@gmail.com>
2019-05-03 23:52:41 -0700

    added ds store deleting and fixed color escape on update script

--- update.sh
+++ update.sh
@@ -1,7 +1,13 @@
-echo -e "\e[31mStarting Scan...\e[0m"
-dpkg-scanpackages -m noDebsHere/ / > Packages
-echo -e "\e[31mComplete...\e[0m"
+echo -e "\033[31mDisplaying DS.Store Files...\033[0m"
+find . -name .DS_Store
+echo -e "\033[31mDeleting...\033[0m"
+find . -name .DS_Store -delete
+echo -e "\033[31mDone...\033[0m"
 
-echo -e "\e[31mBuilding Packages File...\e[0m"
+echo -e "\033[31mStarting Scan...\033[0m"
+dpkg-scanpackages -m noDebsHere/ / > Packages
+echo -e "\033[31mDone...\033[0m"
+
+echo -e "\033[31mBuilding Packages File...\033[0m"
 bzip2 -fks Packages
-echo -e "\e[31mComplete... You can now push to git!!\e[0m"
+echo -e "\033[31mComplete... You can now push to git!!\033[0m"
