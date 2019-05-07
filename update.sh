ba9dc1f33f2c1992cc1b140d5b6afe46344ecd0d
LucifersCircle <luciferscircle@gmail.com>
2019-05-07 08:50:41 -0700

    update compression, made faster but takes more memory to run

--- update.sh
+++ update.sh
@@ -8,6 +8,10 @@ echo -e "\033[31mStarting Scan...\033[0m"
 dpkg-scanpackages -m noDebsHere/ / > Packages
 echo -e "\033[31mDone...\033[0m"
 
-echo -e "\033[31mBuilding Packages File...\033[0m"
-bzip2 -fks Packages
+echo -e "\033[31mCompressing bzip...\033[0m"
+bzip2 -9fkv Packages > Packages.bz2
+echo -e "\033[31mDone...\033[0m"
+
+echo -e "\033[31mCompressing xz...\033[0m"
+xz -9fkev Packages > Packages.xz
 echo -e "\033[31mComplete... You can now push to git!!\033[0m"
