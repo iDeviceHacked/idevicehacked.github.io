13310ef5f3c36390ab4f1c573a7bd2dd3bad6b91
LucifersCircle <luciferscircle@gmail.com>
2019-05-07 20:31:31 -0700

    test

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ find . -name .DS_Store -delete
 echo -e "\033[31mDone...\033[0m"
 
 echo -e "\033[31mStarting Scan...\033[0m"
-#dpkg-scanpackages -m noDebsHere/ / > Packages
+dpkg-scanpackages -m noDebsHere/ / > Packages
 echo -e "\033[31mDone...\033[0m"
 
 echo -e "\033[31mCompressing bzip...\033[0m"
